3ccf6504bb6223a85a3de4c88d028a5586d80e06
Unknown <st01k@lvaas.net>
2018-06-11 09:28:58 -0400

    restructured layout

--- static/renderer.js
+++ static/renderer.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
 // import submit
 document.getElementById("import-submit").addEventListener("click", (event => {
   const { path } = document.getElementById('form-import-file').files[0]
-  M.toast({ html: 'parsing DDL'})
+  M.toast({ html: 'DDL data imported'})
   ipcRenderer.send('file:import', path)
 }))
 
@@ -25,46 +25,71 @@ document.getElementById("export-submit").addEventListener("click", (event => {
   M.toast({ html: 'generating PSV'})
 }))
 
-// record list collapse
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.collapsible.expandable');
-  var instances = M.Collapsible.init(elems, {
-    accordion: false
+function addListListener() {
+  // record list collapse
+  document.addEventListener('DOMContentLoaded', function() {
+    // var elems = document.querySelectorAll('.collapsible.expandable');
+    // var instances = M.Collapsible.init(elems, {
+    //   accordion: false
+    // });
+
+    var elem = document.querySelector('.collapsible.expandable');
+    var instance = M.Collapsible.init(elem, {
+      accordion: false
+    });
   });
-});
+}
 
 // ------------------------------------------------------------------------ IPC
 ipcRenderer.on('file:handler', (event, handler) => {
   clearContent()
   handler_data = handler
   
-  document.getElementById('total-records').innerHTML = `Total Records: ${handler.records.length}`
-  document.getElementById('filename').innerHTML = `${handler.file}`
+  renderFileInfo()
 
-  let div = document.querySelector('#data-list')
+  let div = document.querySelector('#content-body')
   let ul = document.createElement('ul')
-  ul.classList.add('collapsible', 'expandable')
+  ul.classList.add('collapsible', 'expandable', 'popout')
   
   for (let record of handler.records) {
-    let li = document.createElement('li')
-    let head = document.createElement('div')
-    let body = document.createElement('div')
-    
-    head.classList.add('collapsible-header', 'grey', 'darken-2', 'white-text')
-    head.innerHTML = `${record.data.keyword} ${record.data.network} ${record.data.name} ${record.data.description}<br> TYPE: ${record.type}`
-    
-    body.classList.add('collapsible-body', 'grey', 'darken-3', 'white-text', 'truncate')
-    body.innerHTML = `<span><pre>${record.raw}</pre></span>`
-
-    li.appendChild(head)
-    li.appendChild(body)
-    ul.appendChild(li)
+    ul.appendChild(genListItem(record))
   }
   div.appendChild(ul)
 })
 
+// -------------------------------------------------------------------data propogation
+// clears main content and file info
 function clearContent() {
-  document.getElementById('filename').innerHTML = ''
+  document.getElementById('file-name').innerHTML = ''
   document.getElementById('total-records').innerHTML = ''
-  document.getElementById('data-list').innerHTML = ''
+  document.getElementById('content-body').innerHTML = ''
+}
+
+// renders file info
+function renderFileInfo() {
+  let names = handler_data.file.split('/')
+  let name = names[names.length - 1]
+  // document.getElementById('file-name').innerHTML = `${handler.file}`
+  document.getElementById('file-name').innerHTML = `${name}`
+  // document.getElementById('total-records').innerHTML = `Total Records: ${handler.records.length}`
+  document.getElementById('total-records').innerHTML = `Total Records: ${handler_data.records.length}`
+}
+
+// returns list item
+function genListItem(record) {
+  let li = document.createElement('li')
+  let head = document.createElement('div')
+  let body = document.createElement('div')
+  
+  head.classList.add('collapsible-header', 'grey', 'darken-2', 'white-text')
+  // add header icon <i class="material-icons">whatshot</i>
+  head.innerHTML = `${record.data.keyword} ${record.data.network} ${record.data.name} ${record.data.description}<br> TYPE: ${record.type}`
+  
+  body.classList.add('collapsible-body', 'grey', 'darken-3', 'white-text', 'truncate')
+  body.innerHTML = `<span><pre>${record.raw}</pre></span>`
+
+  li.appendChild(head)
+  li.appendChild(body)
+
+  return li
 }
